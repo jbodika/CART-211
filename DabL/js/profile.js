362ef6b402feeeedf491fbcc37cdc6fee0b8f28a
Jolene <joleneodia@hotmail.com>
2024-10-18 16:10:01 -0400

    Removed references to secret

--- DabL/js/profile.js
+++ DabL/js/profile.js
@@ -2,22 +2,21 @@
 const profile = document.querySelector(".profile-card")
 const achievements = document.querySelector(".achievements-card")
 let stories_array = []
-let story_progression= []
+let story_progression = []
 
 // loop through to all the keys stored in localStorage and find those that include 'story' and 'progression_story' to add the them to their respective arrays
-for (var key in localStorage){
-   if (key.startsWith('story')){
+for (var key in localStorage) {
+    if (key.startsWith('story')) {
         stories_array.push(localStorage.getItem(key))
-   }
-   else if(key.startsWith('progression_story_')){
-    story_progression.push(localStorage.getItem(key))
-   }
+    } else if (key.startsWith('progression_story_')) {
+        story_progression.push(localStorage.getItem(key))
+    }
 
- }
+}
 
 
 // check if there's any story progression if there isn't it should display a message instead of the data
-if(stories_array.length==0 ){
+if (stories_array.length == 0) {
     profile.innerHTML += `<p>You haven't read any stories yet!</p>`
     achievements.innerHTML += `<p>You haven't gained any achievements yet!</p>`
 
